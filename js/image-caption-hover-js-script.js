b7431dd29be5cc556ca79743e03a5b4478496e87
ArmonHenry <armon.henry@gmail.com>
2022-04-07 09:56:23 +0100

    test

---
+++ js/image-caption-hover-js-script.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function($) {
+	jQuery('.image-flip-up, .image-flip-down, .rotate-image-down, .tilt-image, .image-flip-right, .image-flip-left').closest('.image-caption-box').css('overflow', 'visible');
+});
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
